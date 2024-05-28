38a4966fc45df7e957d3ff3e9e9630fdb68c50d4
Ana Pastor <apr0050@alu.medac.es>
2024-05-28 12:43:49 +0200

    cambio clase BDD

--- src/main/java/tfc/practica_intermodular/ConexionBaseDatos.java
+++ src/main/java/tfc/practica_intermodular/ConexionBaseDatos.java
@@ -1,3 +1,7 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package tfc.practica_intermodular;
 
 import java.sql.Connection;
@@ -7,56 +11,46 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class ConexionBaseDatos {
+    String url = "jdbc:mysql://localhost:3306/practica_intermodularApr";
+    String usuario = "root";
+    String contraseña = "Med@c";
     
-        // Configuración de la conexión a la base de datos MySQL
-        String url = "jdbc:mysql://localhost:3306/practica_intermodular_BDD_apr";
-        String usuario = "root";
-        String contraseña = "Med@c";
+    //sentencias SQL
+    String sqlMarte = "SELECT * FROM planeta where nombre = 'Marte'";
+    
+    public String[] conectarConsultarMarte() {
+        // Crear array para almacenar los datos
+        String[] datosMarte = new String[7]; 
 
-        /*Sentencias SQL
-        --opcion1: Traer todos los datos de mysql -- menos eficiente
-        --opcion2: Consultar los datos de un planeta específico en la base de datos sin tener que traer todos los datos de una sola vez.  --más eficiente */
-        
-        //consulta Marte
-        String sqlMarte = "SELECT * from Planeta WHERE nombre = 'Marte'";
-        
-    //Metodo Consultar y conectar Marte
-    public String[] conectarConsultarMarte(){
-        
-        //1.Crear array
-        String[] datosMarte = new String[7] ; //creamos array donde se almacenan los datos obtenidos de MYSQL
-        
-        try{
-        //2.Conectarme
-        Connection conexion = DriverManager.getConnection(url, usuario, contraseña);
-        Statement statement = conexion.createStatement();
-        
-        //3.Lanzar consulta
-        ResultSet resultados = statement.executeQuery(sqlMarte);
-        
-        //4.Recoger datos y guardar en array
-        while (resultados.next()) {
-            // Obtener los valores de las columnas
-            datosMarte[0] = resultados.getString("nombre"); //Lo dejo todo string porque nos da el dato igualmente de MySQL
-            datosMarte[1] = resultados.getString("radio_km");
-            datosMarte[2]= resultados.getString("distancia_media_sol_mkm");
-            datosMarte[3] = resultados.getString("periodo_orbital_dias");
-            datosMarte[4]= resultados.getString("temperatura_media_c");
-            datosMarte[5]= resultados.getString("tipo_planeta");
-            datosMarte[6]= resultados.getString("num_satélites");
-            datosMarte[7]= resultados.getString("nombre_estrella");
-        }
-        
-        //5. Cerrar la conexion
-        resultados.close();
-        statement.close();
-        conexion.close();
-        
+        try {
+            // Conectar a la base de datos
+            Connection conexion = DriverManager.getConnection(url, usuario, contraseña);
+            Statement statement = conexion.createStatement();
+
+            // Lanzar consulta
+            ResultSet resultados = statement.executeQuery(sqlMarte);
+
+            // Recoger datos y guardar en el array
+            if (resultados.next()) {
+                datosMarte[0] = resultados.getString("radio_km");
+                datosMarte[1] = resultados.getString("distancia_media_sol_mkm");
+                datosMarte[2] = resultados.getString("periodo_orbital_dias");
+                datosMarte[3] = resultados.getString("temperatura_media_c");
+                datosMarte[4] = resultados.getString("tipo_planeta");
+                datosMarte[5] = resultados.getString("num_satelites");
+                datosMarte[6] = resultados.getString("nombre_estrella");
+            }
+
+            // Cerrar la conexión
+            resultados.close();
+            statement.close();
+            conexion.close();
+
         } catch (SQLException e) {
-        e.printStackTrace();
+            e.printStackTrace();
         }
-        
-        //6.Return array
+
+        // Devolver el array
         return datosMarte;
     }
 }
